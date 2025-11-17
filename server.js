c25af094deb6bdb6f526e9b14f8693530103b6f4
Olena Kast <olenakast@gmail.com>
2025-11-17 15:57:45 +0200

    feat: Complete final testing of the API

--- server.js
+++ server.js
@@ -5,25 +5,18 @@ const PORT = 3000;
 
 app.use(express.json());
 
-// --- MIDDLEWARE (ОХОРОНА та ЛОГИ) ---
+// --- MIDDLEWARE ---
 
-// 1. ЛОГЕР (Записує кожен запит) - ДОДАНО
 const loggingMiddleware = (req, res, next) => {
   const timestamp = new Date().toISOString();
   const method = req.method;
   const url = req.url;
-  
-  // Виводимо в консоль: [Час] МЕТОД АДРЕСА
   console.log(`[${timestamp}] ${method} ${url}`);
-  
   next();
 };
 
-// Підключаємо логер ГЛОБАЛЬНО (перед усіма маршрутами)
 app.use(loggingMiddleware);
 
-
-// 2. Перевірка логіна і пароля
 const authMiddleware = (req, res, next) => {
   const login = req.headers['x-login'];
   const password = req.headers['x-password'];
@@ -37,7 +30,6 @@ const authMiddleware = (req, res, next) => {
   next();
 };
 
-// 3. Перевірка на Адміна
 const adminOnlyMiddleware = (req, res, next) => {
   if (!req.user || req.user.role !== 'admin') {
     return res.status(403).json({ message: 'Access denied. Admin role required.' });
@@ -52,17 +44,36 @@ app.get('/documents', authMiddleware, (req, res) => {
 });
 
 app.post('/documents', authMiddleware, (req, res) => {
-  const newDocument = req.body;
-  newDocument.id = Date.now();
+  const { title, content } = req.body;
+  
+  if (!title || !content) {
+    return res.status(400).json({ message: 'Bad Request. Fields "title" and "content" are required.' });
+  }
+
+  const newDocument = {
+    id: Date.now(),
+    title,
+    content,
+  };
   documents.push(newDocument);
   res.status(201).json(newDocument);
+});
+
+app.delete('/documents/:id', authMiddleware, (req, res) => {
+    const documentId = parseInt(req.params.id);
+    const documentIndex = documents.findIndex(doc => doc.id === documentId);
+
+    if (documentIndex === -1) {
+        return res.status(404).json({ message: 'Document not found' });
+    }
+
+    documents.splice(documentIndex, 1);
+    res.status(204).send();
 });
 
 app.get('/employees', authMiddleware, adminOnlyMiddleware, (req, res) => {
   res.status(200).json(employees);
 });
-
-// --- ЗАПУСК ---
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
